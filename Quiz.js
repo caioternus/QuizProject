a0a80d408ef9f6c47d7b6f788c03bd4d62f2a0ba
caioternus <55170110+caioternus@users.noreply.github.com>
2025-03-12 11:41:09 -0300

    Update Quiz.js

--- Quiz.js
+++ Quiz.js
@@ -81,11 +81,12 @@ const showQuestion = (qNumber) => {
     answersContainer.innerHTML = data[qNumber].answers
         .map(
             (item, index) =>
-
-                `<div class="answer">
+                `
+                <div class="answer">
                     <input type="radio" id=${index} name="answer" value=${item.isCorrect} />
                     <label for="1">${item.answer}</label>
-                </div>`
+                </div>
+                `
         )
         .join("");
     selectAnswer();
